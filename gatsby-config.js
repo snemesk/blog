5f3f3b6f8e96548366a02e1d766455b4c55e7381
snemesk <sainemesis.ksk@gmail.com>
2020-03-07 02:05:04 +0900

    firstcomit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `sukeneme blog`,
+    author: `sukeneme`,
+    description: `すけねめのブログです`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/suke4mb`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/snemesk`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    すけねめです <Styled.a href="https://twitter.com/suke4mb">Twitterはこちら</Styled.a>
+    {` `}
+
+  </Fragment>
+)
